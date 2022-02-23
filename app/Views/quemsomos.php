8b28b9aa9ac4f7ebe62d981d6648f442c2b7d528
paulocoppe10 <paulocoppe10@gmail.com>
2022-02-23 14:49:09 -0300

    criando sistema de template

---
+++ app/Views/quemsomos.php
@@ -0,0 +1,3 @@
+<div class="col">
+    <h1>quem somos<h1>
+</div>
--- app/Views/template/header.php
+++ app/Views/template/header.php
@@ -26,10 +26,9 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    <a class="nav-link" href="#">Features</a>
-                    <a class="nav-link" href="#">Pricing</a>
-                    <a class="nav-link disabled">Disabled</a>
+                    <a class="nav-link active" aria-current="page" href="home">Home</a>
+                    <a class="nav-link" href="quemsomos">quemsomos</a>
+                    <a class="nav-link" href="contato">contato</a>
                 </div>
                 </div>
             </div>
